b549f992d1ef1bacef0c6494c82e5e2aeac47c91
tesujiro <tesujiro@gmail.com>
2019-01-21 09:46:41 +0900

    add: test db_scan

--- data/db/database_test.go
+++ data/db/database_test.go
@@ -96,3 +96,75 @@ func TestSetJson(t *testing.T) {
 		}
 	}
 }
+
+func TestScan(t *testing.T) {
+	data := []struct {
+		key      string
+		id       string
+		lat, lon float64
+		time     float64
+	}{
+		{key: "test-key1", id: "id1", lat: 0, lon: 0, time: 100},
+		{key: "test-key1", id: "id2", lat: 1.23, lon: 4.56, time: 10},
+		{key: "test-key1", id: "id3", lat: 1, lon: 2, time: 500},
+		{key: "test-key1", id: "id4", lat: 0, lon: 0, time: 0},
+	}
+	tests := []struct {
+		key         string
+		args        []interface{}
+		expectedLen int
+	}{
+		{key: "test-key1", expectedLen: 4},
+		{key: "test-key2", expectedLen: 0},
+		{key: "test-key1", args: []interface{}{"WHERE", "time", 0, 100}, expectedLen: 3},
+		{key: "test-key1", args: []interface{}{"WHERE", "time", 300, 500}, expectedLen: 1},
+		{key: "test-key2", args: []interface{}{"WHERE", "time", 0, 100}, expectedLen: 0},
+	}
+
+	c := pool.Get()
+	defer c.Close()
+	for _, record := range data {
+		feature := &GeoJsonFeature{ // [0]
+			Type: "Feature",
+			Geometry: &Geometry{
+				Type:        "Point",
+				Coordinates: [2]float64{record.lon, record.lat},
+			},
+			Properties: map[string]interface{}{
+				"id":   record.id,
+				"time": record.time,
+			},
+		}
+		var test_json string
+		b, err := json.Marshal(feature)
+		if err != nil {
+			t.Fatalf("JSON Marshal error: %v", err)
+		}
+		test_json = string(b)
+		if err := db_set_json(c, record.key, record.id, test_json, "FIELD", "time", record.time); err != nil {
+			t.Fatalf("SET DB error: %v", err)
+		}
+	}
+	for i, test := range tests {
+		features, err := db_scan(c, test.key, test.args...)
+		if err != nil {
+			t.Fatalf("SCAN DB error: %v", err)
+		}
+		if len(features) != test.expectedLen {
+			t.Errorf("Case:[%v] received: %v - expected: %v", i, len(features), test.expectedLen)
+		}
+	}
+}
+
+/*
+	// Test WITHIN BOUNDS
+	testBounds := []struct {
+		s, w, n, e float64
+		key        string
+		args       []interface{}
+		expected   int
+	}{
+		{s: 0, w: 0, n: 0, e: 0, key: "key1", expected: 1},
+		{s: 0, w: 0, n: 0, e: 0, key: "key2", expected: 0},
+	}
+*/
